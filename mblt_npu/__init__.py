2a1aa881463d378d3282dc713f56584b7488eefd
jinman <jinman@mobilint.com>
2026-08-26 09:40:22 +0900

    Bump version to 0.0.2 in __init__.py

--- mblt_npu/__init__.py
+++ mblt_npu/__init__.py
@@ -22,7 +22,7 @@ from .npu_backend import (
 from .logging import log_model_details
 from .onnx_backend import ONNXBackend
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 __all__ = [
     "BACKEND_CLASSES",
